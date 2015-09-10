35796799cf895b3dd8d7bda3dd3b2c92a7e00a4b
qytian <fdqiyuantian@gmail.com>
2015-09-09 17:18:50 -0700

    Move everything to the OLD-2015-09-09 folder and started using the downsampling scheme and sharing the codes with Li Ye

--- G3d.m
+++
@@ -1,7 +0,0 @@
-function Gker = G3d(sigma)
-halfsize = ceil(3 * sigma);
-x = -halfsize : halfsize;
-[X, Y, Z] = meshgrid(x, x, x);
-r = sqrt(X.*X + Y.*Y + Z.*Z);
-Gker = exp(-r.*r / 2 / sigma / sigma) / sigma / sqrt(2 * pi);
-end
--- dog_new-OLD.m
+++
@@ -1,6 +0,0 @@
-function k_dog = dog_new(sigma)
-halfsize = ceil(3 * sigma);
-x = [-halfsize : halfsize];
-k_dog = -x .* exp(-x.*x / 2 / sigma / sigma) / sigma^3 / sqrt(2 * pi);
-
-end
